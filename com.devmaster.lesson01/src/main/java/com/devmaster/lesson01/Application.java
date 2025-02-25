81d17b738a9acaebdedf48dee57c2a08b6b8704f
tvchung <hna.tvchung@gmail.com>
2025-02-25 19:26:09 +0700

    Lesson03

--- com.devmaster.lesson01/src/main/java/com/devmaster/lesson01/Application.java
+++ com.devmaster.lesson01/src/main/java/com/devmaster/lesson01/Application.java
@@ -2,6 +2,9 @@ package com.devmaster.lesson01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Application {
@@ -9,6 +12,18 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("Devmaster Hello World");
+		MyBean obj = new MyBean();
+		System.out.println(obj.sayHello());
 	}
 
+}
+class MyBean{
+	@Bean
+	public MyBean myBean(){
+		return new MyBean();
+	}
+	@Bean
+	public String sayHello(){
+		return "Hello World";
+	}
 }
